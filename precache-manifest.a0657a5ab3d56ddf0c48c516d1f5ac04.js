172e7c00f178814211e03c6597c1f19ed6c0db70
JuanGF <jugaybre@gmail.com>
2020-07-02 13:26:06 -0500

    Updates

---
+++ precache-manifest.a0657a5ab3d56ddf0c48c516d1f5ac04.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e46baf74e43b01d0d4858e973d5841",
+    "url": "/React_Exercices/index.html"
+  },
+  {
+    "revision": "215373575239667b59a1",
+    "url": "/React_Exercices/static/css/main.395c4ded.chunk.css"
+  },
+  {
+    "revision": "7fdd390bc9ecbe26a105",
+    "url": "/React_Exercices/static/js/2.e6859db5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Exercices/static/js/2.e6859db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215373575239667b59a1",
+    "url": "/React_Exercices/static/js/main.850128c4.chunk.js"
+  },
+  {
+    "revision": "33ef81239fd1ae48d416",
+    "url": "/React_Exercices/static/js/runtime-main.6e4becd9.js"
+  },
+  {
+    "revision": "82a9b053a817b69913a6bd7b49aed756",
+    "url": "/React_Exercices/static/media/FalconHeavy.82a9b053.jpg"
+  },
+  {
+    "revision": "7a8de5570fcea476969fc3c5a925ca8c",
+    "url": "/React_Exercices/static/media/T-mec.7a8de557.jpg"
+  },
+  {
+    "revision": "d5d8e16a3c36e18e42cee17bd5a3a5f6",
+    "url": "/React_Exercices/static/media/analisis.d5d8e16a.png"
+  },
+  {
+    "revision": "8fb60edd51ed71242cca1731c8c2725e",
+    "url": "/React_Exercices/static/media/cat.8fb60edd.png"
+  },
+  {
+    "revision": "f9e7bb67910b9ab401cc4aa47f14a8e0",
+    "url": "/React_Exercices/static/media/consultoria.f9e7bb67.png"
+  },
+  {
+    "revision": "39701d7abf76f146a563c0a6f4eb9bf4",
+    "url": "/React_Exercices/static/media/corpSolutions.39701d7a.png"
+  },
+  {
+    "revision": "2b6fd9910689d4ccff2cfbd70cbaa49a",
+    "url": "/React_Exercices/static/media/easyResponsive.2b6fd991.png"
+  },
+  {
+    "revision": "2fa3729fa70e14a3107f859d4d596713",
+    "url": "/React_Exercices/static/media/formacion.2fa3729f.png"
+  },
+  {
+    "revision": "e87b2917cd756affe0071fe54bd46c83",
+    "url": "/React_Exercices/static/media/investigacion.e87b2917.png"
+  },
+  {
+    "revision": "da5035524f5fe08a7fc1057430c43a2f",
+    "url": "/React_Exercices/static/media/newYorkTimes.da503552.png"
+  },
+  {
+    "revision": "5227ab329b2bb269290a0c50a4b4475d",
+    "url": "/React_Exercices/static/media/person.5227ab32.png"
+  },
+  {
+    "revision": "c8a7fc848876077a2fa7958c4440ffd6",
+    "url": "/React_Exercices/static/media/webPage.c8a7fc84.png"
+  }
+]);
